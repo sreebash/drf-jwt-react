2264386f6d2975151d7e2e78ba3e033351c8fe11
Sreebash <sreebashdas.1@gmail.com>
2020-11-13 19:31:08 +0600

    setup integrate login and register form

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,15 +1,49 @@
 import React, {Component} from 'react';
-import Navbar from "../Navbar/Navbar";
+import {Form, Col} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 class Login extends Component {
     render() {
         return (
-            <div>
-                  <br/><br/><br/>
-                <h2>Login</h2>
+            <div><br/><br/><br/>
 
-                <input type="text"/>
+                <div>
+                    <div className="container">
+
+                        <h1 className="mt-4 mb-3">Login
+                            <small>Form</small>
+                        </h1>
+                        <ol className="breadcrumb">
+                            <li className="breadcrumb-item active">Login</li>
+                        </ol>
+                        <div className="row container">
+                            <form>
+
+                                <div className="form-group">
+                                    <label>Email:</label>
+                                    <input type="email" placeholder="Email address" className="form-control"/>
+                                </div>
+
+                                <div className="form-group">
+                                    <label>Password:</label>
+                                    <input type="password" className="form-control" placeholder="Password"/>
+                                </div>
+
+
+                                <button type="submit" className="btn btn-primary btn-block">Log In</button>
+                                <p className="forgot-password text-right">
+                                    Need an account? <Link to="/register">Sign Up</Link>
+                                </p>
+                            </form>
+                        </div>
+                        <br/><br/>  <br/>
+
+
+                    </div>
+                </div>
+
             </div>
+
         );
     }
 }
--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,14 +1,57 @@
 import React, {Component} from 'react';
 import Navbar from "../Navbar/Navbar";
+import {Link} from "react-router-dom";
 
 class Register extends Component {
     render() {
         return (
-            <div>
-                <br/><br/><br/>
-                <h2>Register</h2>
+            <div><br/><br/><br/>
 
-                <input type="text"/>
+                <div>
+                    <div className="container">
+
+                        <h1 className="mt-4 mb-3">Register
+                            <small>Form</small>
+                        </h1>
+                        <ol className="breadcrumb">
+                            <li className="breadcrumb-item active">Sign Up</li>
+                        </ol>
+                        <div className="row container">
+                            <form>
+
+                                <div className="form-group">
+                                    <label>First name:</label>
+                                    <input type="text" placeholder="First Name" className="form-control"/>
+                                </div>
+
+                                <div className="form-group">
+                                    <label>Last name:</label>
+                                    <input type="email" placeholder="Last Name" className="form-control"/>
+                                </div>
+
+                                <div className="form-group">
+                                    <label>Email:</label>
+                                    <input type="email" placeholder="Email address" className="form-control"/>
+                                </div>
+
+
+                                <div className="form-group">
+                                    <label>Password:</label>
+                                    <input type="password" className="form-control" placeholder="Password"/>
+                                </div>
+
+
+                                <button type="submit" className="btn btn-primary btn-block">Log In</button>
+                                <p className="forgot-password text-right">
+                                    Have an account? <Link to="/login">Log In</Link>
+                                </p>
+                            </form>
+                        </div>
+                        <br/><br/> <br/>
+
+
+                    </div>
+                </div>
 
             </div>
         );
